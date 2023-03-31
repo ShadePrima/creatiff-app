d2e5b955a5fe0cda346fbd42efab2bcad0e6d667
ShadePrima <aleksandrag1984@gmail.com>
2023-03-31 21:40:41 +0300

    partners section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Partners from './components/Partners';
 import './styles/main.scss';
 
 function App() {
   return (
     <div>
       <Header />
+      <Partners />
     </div>
   );
 }
---
+++ src/components/Partners/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './Partners.module.scss';
+import partnerIcon from '../../assets/Partners/partner.png';
+
+const partners = [
+  { id: 1, icone: partnerIcon },
+  { id: 2, icone: partnerIcon },
+  { id: 3, icone: partnerIcon },
+  { id: 4, icone: partnerIcon },
+  { id: 5, icone: partnerIcon },
+  { id: 6, icone: partnerIcon },
+];
+
+const Partners = () => {
+  return (
+    <div className={styles.root}>
+      <div className={`${'wrapper'} ${styles.container}`}>
+        <div>
+          <p>Наші Партнери:</p>
+          {partners.map((partner) => (
+            <>
+              <img src={partner.icone} alt={partner.name} />
+            </>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Partners;

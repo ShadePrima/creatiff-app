e693c497ba3a4bc7cfa5d5ff475a39e245e546dc
ShadePrima <aleksandrag1984@gmail.com>
2023-04-02 10:39:23 +0300

    catalog click

--- src/components/CatalogItem/index.jsx
+++ src/components/CatalogItem/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import cx from 'clsx';
+
 import catalogCheckTop from '../../assets/icons/check-top-icon.png';
+import catalogCheckBottom from '../../assets/icons/check-bottom-icon.png';
 import catalogIcon from '../../assets/icons/icon-background.png';
 
 import styles from './CatalogItem.module.scss';
@@ -7,18 +10,37 @@ import styles from './CatalogItem.module.scss';
 const CatalogItem = ({ id, title, handleMoreClick, isActive }) => {
   console.log(isActive, 'isActive');
   return (
-    <div className={styles.catalogItem}>
+    <div
+      onClick={() => handleMoreClick(id)}
+      className={cx(styles.catalogItem, isActive && styles.openItem)}
+    >
       <div className={styles.leftConteiner}>
         <img src={catalogIcon} alt='Icon' className={styles.catalogIcon} />
-        <div className={styles.catalogTitle}>{title}</div>
-        {isActive && <p>test</p>}
+        <div className={cx(isActive && styles.titleConteiner)}>
+          <div className={styles.catalogTitle}>{title}</div>
+          {isActive && (
+            <p className={styles.catalogDescription}>
+              Серед наших типов вікон маємо: вхідні, міжкімнатні,
+              металопластикові
+            </p>
+          )}
+        </div>
       </div>
-      <img
-        onClick={() => handleMoreClick(id)}
-        src={catalogCheckTop}
-        alt='Check Top'
-        className={styles.catalogCheck}
-      />
+      {isActive ? (
+        <img
+          onClick={() => handleMoreClick(id)}
+          src={catalogCheckTop}
+          alt='Check Top'
+          className={styles.catalogCheck}
+        />
+      ) : (
+        <img
+          onClick={() => handleMoreClick(id)}
+          src={catalogCheckBottom}
+          alt='Check Top'
+          className={styles.catalogCheck}
+        />
+      )}
     </div>
   );
 };

afe97128679e0aa0393a23bb22998bb6e236b961
ShadePrima <aleksandrag1984@gmail.com>
2023-04-13 12:05:14 +0300

    fix header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import React from 'react';
+import React from 'react'
 
-import Advantages from './components/Advantages';
-import Catalog from './components/Catalog';
-import CataloBlock from './components/CatalogBlock';
-import ContactForm from './components/ContactForm';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Partners from './components/Partners';
-import Services from './components/Services';
+import Advantages from './components/Advantages'
+import Catalog from './components/Catalog'
+import CataloBlock from './components/CatalogBlock'
+import ContactForm from './components/ContactForm'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Partners from './components/Partners'
+import Services from './components/Services'
 
-import './styles/main.scss';
+import './styles/main.scss'
 
 function App() {
   return (
     <div>
       <Header />
-      <Partners />
-      <Services />
-      <Catalog />
+      {/* <Catalog />
       <CataloBlock />
       <Advantages />
       <ContactForm />
-      <Footer />
+      <Footer /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
